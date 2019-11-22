9c42f34b86cac85003458cf0f8c4c00f19c6be0e
Andres Rojas <afro17@gmail.com>
2019-11-22 09:56:15 -0500

    Project Setup

---
+++ CurrencyConverter/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Andres Rojas on 22/11/19.
+//  Copyright © 2019 Andres Rojas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
